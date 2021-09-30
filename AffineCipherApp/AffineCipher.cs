5ce96a1908722b68e2295f41c9358e62097c7e7a
Marek Olšák <m_olsak@utb.cz>
2021-09-30 22:51:46 +0200

    Major encryption mechanism fix #1

--- AffineCipherApp/AffineCipher.cs
+++ AffineCipherApp/AffineCipher.cs
@@ -390,9 +390,8 @@ namespace AffineCipherApp
         /// <returns>Encrypted characters index to be used to instantiate encryption dictionary.</returns>
         private static int EncryptionMechanism(in int input, ref int a, ref int b, ref int m)
         {
-            int encryptedVal = a * input + Mod(ref b, ref m);
-            if (encryptedVal < m) return encryptedVal;
-            return encryptedVal % m;
+            int temp = a * input + b;
+            return Mod(ref temp, ref m);
         }
 
         /// <summary>
